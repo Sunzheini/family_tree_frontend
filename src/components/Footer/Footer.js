c07e7f1c45779498f415b6a2fd89b43cfb13ea47
Sunzheini <daniel_zorov@abv.bg>
2025-01-27 16:47:09 +0200

    Added a footer.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(props) {
+    return (
+        <footer className="footer">
+            <h3 className="footer-title">{props.content}</h3>
+        </footer>
+    );
+}
